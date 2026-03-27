d1029c01b2cb16d2fae3db1025b0637a0cb054c7
wjdmini <wjdmini1226@naver.com>
2026-03-27 17:42:17 +0900

    git pull develop

--- final-project-springboots/src/main/java/com/springbootstudy/bbs/domain/ItemVO.java
+++ final-project-springboots/src/main/java/com/springbootstudy/bbs/domain/ItemVO.java
@@ -9,17 +9,6 @@ import lombok.Data;
 @Data
 public class ItemVO {
 
-<<<<<<< HEAD
-  private Long itemIdx; // PK
-  private String itemName; // 상품명
-  private Integer itemCategoryIdx; // FK → item_category
-  private String itemBrand; // 브랜드
-  private String itemCondition; // 상태 (NEW / USED_A / USED_B 등)
-  private String itemThumbnailImg; // 썸네일 이미지
-  private String itemDetailImg; // 상세 이미지
-  private java.time.LocalDateTime itemRegdate; // 등록일
-  private String itemIsDeleted; // 'Y' / 'N'
-=======
  private Long    		itemIdx;           // PK
  private String  		itemName;          // 상품명
  private Integer 		itemCategoryIdx;   // FK → item_category
@@ -29,6 +18,5 @@ public class ItemVO {
  private String  		itemDetailImg;     // 상세 이미지
  private LocalDateTime 	itemRegdate;   	   // 등록일
  private String  		itemIsDeleted;     // 'Y' / 'N'
->>>>>>> 79d91f471be66091855e1ab616e702897bc0f5e1
 
 }
