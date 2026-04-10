8211d35bf938546d69bb606ca8add5e145c6988a
wjdmini <wjdmini1226@naver.com>
2026-04-10 14:37:47 +0900

    pay mapping edit

--- final-project-springboots/src/main/java/com/springbootstudy/bbs/controller/MemberAddrController.java
+++ final-project-springboots/src/main/java/com/springbootstudy/bbs/controller/MemberAddrController.java
@@ -9,56 +9,54 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.springbootstudy.bbs.domain.MemberAddrVO;
 import com.springbootstudy.bbs.domain.MemberVO;
 import com.springbootstudy.bbs.service.MemberAddrService;
-import com.springbootstudy.bbs.service.MemberService;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MemberAddrController {
 
-	@Autowired
-	private MemberAddrService memberAddrService;
-	
-	// 주소등록창 -----------------------------------------------------------------
-	
-	@GetMapping("/members/memberAddrInsert") 
-	public String memberAddrInsert(HttpSession session, Model model) {
+    @Autowired
+    private MemberAddrService memberAddrService;
 
-	    MemberVO loginMember = (MemberVO) session.getAttribute("loginUser");
+    // 주소등록창 -----------------------------------------------------------------
 
-	    if (loginMember == null) {
-	        return "redirect:/members/login"; 
-	    }
+    @GetMapping("/members/memberAddrInsert")
+    public String memberAddrInsert(HttpSession session, Model model) {
 
-	    model.addAttribute("member", loginMember);  
+        MemberVO loginMember = (MemberVO) session.getAttribute("loginUser");
 
-	    return "/views/member/memberAddrInsert";  
-	}
-	
-	// memberAddrUpdate.html 의 Ajax 저장용
-	@PostMapping("/member/insertAddrAjax.do")
-	@ResponseBody
-	public int insertAddr(MemberAddrVO vo) {
+        if (loginMember == null) {
+            return "redirect:/members/login";
+        }
 
-		// 두개 확인 필요
-	    int result = memberAddrService.registerAddr(vo);
-	    vo.setIsPrimary("N");
-	    
-	    return result;
-	}
+        model.addAttribute("member", loginMember);
 
-	// 주소목록창 -----------------------------------------------------------------
-	
-	// 저장된 값 보여주기 
-    @GetMapping("/members/memberAddr") 
+        return "/views/member/memberAddrInsert";
+    }
+
+    // memberAddrUpdate.html 의 Ajax 저장용
+    @PostMapping("/member/insertAddrAjax.do")
+    @ResponseBody
+    public int insertAddr(MemberAddrVO vo) {
+
+        // 두개 확인 필요
+        int result = memberAddrService.registerAddr(vo);
+        vo.setIsPrimary("N");
+
+        return result;
+    }
+
+    // 주소목록창 -----------------------------------------------------------------
+
+    // 저장된 값 보여주기
+    @GetMapping("/members/addresses")
     public String memberAddr(HttpSession session, Model model) {
 
-    	// 로그인 안했으면 쫓아내기
+        // 로그인 안했으면 쫓아내기
         MemberVO loginMember = (MemberVO) session.getAttribute("loginUser");
         if (loginMember == null) {
             return "redirect:/members/login";
@@ -68,49 +66,46 @@ public class MemberAddrController {
         model.addAttribute("addrList", addrList);
         model.addAttribute("member", loginMember);
 
-        return "/views/member/memberAddr"; 
-    } 
-
+        return "/views/member/memberAddr";
+    }
 
     // 주소 삭제 -----------------------------------------------------------------
-    
+
     @PostMapping("/members/addrDelete")
     public String deleteAddr(@RequestParam("addrIdx") Long addrIdx) {
 
         memberAddrService.deleteAddr(addrIdx);
 
-        return "redirect:/members/memberAddr"; 
+        return "redirect:/members/memberAddr";
     }
-    
-    
+
     // 주소 수정 -----------------------------------------------------------------
-    
+
     @GetMapping("/members/memberAddrUpdate")
-    public String updateAddrForm(@RequestParam("addrIdx") Long addrIdx,HttpSession session, Model model) { 
-    	
-    	// 로그인 정보 받아오기
+    public String updateAddrForm(@RequestParam("addrIdx") Long addrIdx, HttpSession session, Model model) {
+
+        // 로그인 정보 받아오기
         MemberVO loginMember = (MemberVO) session.getAttribute("loginUser");
-        
+
         // 로그인 안했으면 쫓아내기
         if (loginMember == null) {
-            return "redirect:/members/login"; 
+            return "redirect:/members/login";
         }
-        
+
         // 주소가 없어도 쫓아내기
         if (addrIdx == null) {
             return "redirect:/members/memberAddr";
         }
-        
+
         MemberAddrVO addr = memberAddrService.selectOne(addrIdx);
-    	
+
         // 로그인 세션
-    	model.addAttribute("member", loginMember); 
-    	model.addAttribute("addr", addr);
-    	
-    	return "/views/member/memberAddrUpdate"; 
+        model.addAttribute("member", loginMember);
+        model.addAttribute("addr", addr);
+
+        return "/views/member/memberAddrUpdate";
     }
-    
-    
+
     @PostMapping("/member/updateAddrAjax.do")
     @ResponseBody
     public int updateAddr(MemberAddrVO vo) {
